d0aa141d0326d830ea3793459971299ea979b6f4
Petter <pettermailyahoo.com>
2024-05-15 09:44:59 +0200

    extend oppgave2

---
+++ Oppgaver/Oppgave2 - Routing/løsningsforslag/oppgave2/nesteNivaa/page.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import BackButton from "@/src/common/BackButton";
+import {RowAlignContainer} from "@/src/common/RowAlignContainer";
+import React from "react";
+
+export default function NesteNivaa() {
+
+    return (
+        <main>
+            <h1>Oppgave 2 - Neste nivå</h1>
+            <RowAlignContainer border={false}>
+                <BackButton/>
+            </RowAlignContainer>
+        </main>
+    );
+}
