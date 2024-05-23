2455973615bdd556327c10be4e5e8cf3b97d26c4
Petter <pettermailyahoo.com>
2024-05-23 11:09:46 +0200

    refine opg5

--- Oppgaver/Oppgave5 - Data Fetching fra Vercel PostgresSQL/løsningsforslag/oppgave5/page.tsx
+++ Oppgaver/Oppgave5 - Data Fetching fra Vercel PostgresSQL/løsningsforslag/oppgave5/page.tsx
@@ -1,17 +1,22 @@
 import { sql } from "@vercel/postgres";
 import Image from "next/image";
+import BackButton from "@/src/common/back-button/BackButton";
+import React from "react";
 
 export default async function Oppgave5(){
     const { rows } = await sql`SELECT id, brand, image_url from cars`;
 
     return (
-        <div>
+        <main>
+            <div>Antall biler: {rows?.length}</div>
+            <BackButton/>
+
             {rows.map((row) => (
                 <div key={row.id}>
                     {row.brand}
                     <Image src={row.image_url} width={500} height={500} alt={`Image of ${row.brand} car`}/>
                 </div>
             ))}
-        </div>
+        </main>
     );
 }
